ac2564d7f219225f07536fc9f49c456842963c9b
Kodeine99 <tung.nv2510@gmail.com>
2020-10-01 22:16:19 +0700

    Add ZingChart functional stateless component

---
+++ Reactjs/FunctionalComponents/ZingChartComponent/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ZingChart from "./components/ZingChart";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ZingChart />
+    </div>
+  );
+}
---
+++ Reactjs/FunctionalComponents/ZingChartComponent/src/components/ZingChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./ZingChart.css";
+
+export default function ZingChart() {
+  return (
+    <div className="ZingChart">
+      <div className="ZingChart-img"></div>
+    </div>
+  );
+}
